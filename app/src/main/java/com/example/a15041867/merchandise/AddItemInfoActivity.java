5498ea127620c9121e9483d4d213a37ab2a2729d
RX101 <15041867@myrp.edu.sg>
2017-06-06 22:02:18 +0800

    completed

--- app/src/main/java/com/example/a15041867/merchandise/AddItemInfoActivity.java
+++ app/src/main/java/com/example/a15041867/merchandise/AddItemInfoActivity.java
@@ -2,12 +2,45 @@ package com.example.a15041867.merchandise;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class AddItemInfoActivity extends AppCompatActivity {
+
+    private EditText etItemName1, etPrice1,etQuantity1;
+    private Button btnAdd;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_item_info);
+        etItemName1 = (EditText)findViewById(R.id.editTextItemName1);
+        etPrice1 = (EditText)findViewById(R.id.editTextPrice1);
+        etQuantity1 = (EditText)findViewById(R.id.editTextQuantity1);
+        btnAdd = (Button)findViewById(R.id.addDetailsButton);
+    }
+
+    public void addNewRecordButtonClicked(View view){
+        etItemName1 = (EditText)findViewById(R.id.editTextItemName1);
+        etPrice1 = (EditText)findViewById(R.id.editTextPrice1);
+        etQuantity1 = (EditText)findViewById(R.id.editTextQuantity1);
+
+        //TODO 02: Send the HttpRequest to createNewEntry.php
+        HttpRequest request = new HttpRequest("http://10.0.2.2/C302_P07/addItem.php");
+        request.setMethod("POST");
+        request.addData("item_name",etItemName1.getText().toString());
+        request.addData("quantity",etQuantity1.getText().toString());
+        request.addData("price",etPrice1.getText().toString());
+        request.execute();
+
+
+        try{
+            finish();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 }
--- app/src/main/java/com/example/a15041867/merchandise/DisplayItemInfoActivity.java
+++ app/src/main/java/com/example/a15041867/merchandise/DisplayItemInfoActivity.java
@@ -3,6 +3,7 @@ package com.example.a15041867.merchandise;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -30,6 +31,8 @@ public class DisplayItemInfoActivity extends AppCompatActivity {
         etItemName = (EditText)findViewById(R.id.editTextItemName);
         etPrice = (EditText)findViewById(R.id.editTextPrice);
         etQuantity = (EditText)findViewById(R.id.editTextQuantity);
+        btnDelete = (Button)findViewById(R.id.deleteButton);
+        btnUpdate = (Button)findViewById(R.id.updateButton);
 
         Intent intent = getIntent();
         userId = intent.getStringExtra("com.example.MAIN_MESSAGE");
@@ -48,6 +51,49 @@ public class DisplayItemInfoActivity extends AppCompatActivity {
 
         }
         catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void updateRecordButtonClicked(View view){
+
+        etItemName = (EditText)findViewById(R.id.editTextItemName);
+        etPrice = (EditText)findViewById(R.id.editTextPrice);
+        etQuantity = (EditText)findViewById(R.id.editTextQuantity);
+
+        //TODO 03: Send the HttpRequest to updateContact.php
+        HttpRequest request = new HttpRequest("http://10.0.2.2/C302_P07/updateItemById.php");
+        request.setMethod("POST");
+        request.addData("id",userId);
+        request.addData("item_name",etItemName.getText().toString());
+        request.addData("quantity",etQuantity.getText().toString());
+        request.addData("price",etPrice.getText().toString());
+        request.execute();
+
+
+
+
+        try{
+            finish();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void deleteRecordButtonClicked(View view){
+        //TODO 04: Send HttpRequest to removeContact.php
+        HttpRequest request = new HttpRequest("http://10.0.2.2/C302_P07/deleteItem.php");
+        request.setMethod("POST");
+        request.addData("Id",userId);
+        request.execute();
+
+
+
+        try{
+
+
+            finish();
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
